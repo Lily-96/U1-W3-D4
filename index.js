4102d34e158318db93a207d2e49cf202126ae72f
Liliya Savitska <liliyasavitska@gmail.com>
2025-03-28 01:32:18 +0100

    .

--- index.js
+++ index.js
@@ -10,4 +10,15 @@ for (let i = 0; i <= 89; i++) {
   tombola.appendChild(newDiv);
 }
 
-const box = document.createElement("h3");
+const getRandomNum = function (range) {
+  const randIndex = Math.floor(Math.random() * range.length);
+  const random = range.splice(randIndex, 1)[0];
+  return random;
+};
+const generateRandNumber = function (range) {
+  const random = getRandomNum(range);
+  const randNumDiv = document.getElementById("randNum");
+  randNumDiv.innerText = "Numero: " + random;
+  const cells = document.querySelectorAll(".tombola");
+  cells[random - 1].classList.add("highlight");
+};
